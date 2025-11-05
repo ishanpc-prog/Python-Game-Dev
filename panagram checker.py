83aa5c7b6c9617aa0015503935ff1826f25a4641
ishanpc-prog <ishanpc02@gmail.com>
2025-11-05 16:31:01 +0100

    Add files via upload

--- panagram checker.py
+++ panagram checker.py
@@ -1,4 +1,4 @@
-word = str(input("Enter String")).lower()
+word = str(input("Enter String ")).lower()
 letters = {}
 for c in word:
     if c.isalpha():
@@ -6,4 +6,13 @@ for c in word:
             letters[c] = letters[c] + 1 #increasing the value count of a letter which  is also in the dictionary
         else:
           letters[c] = 1 
-print (letters)           
+print (letters)
+
+p = True
+for i in letters.values():
+    if i == 0:
+        p = False
+if p == False:
+    print("It isn't a panagram.")
+else:
+    print("It is a panagram")    
