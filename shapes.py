160272919bd9bc52e328d47b195a07df5b4e24a0
ishanpc-prog <ishanpc02@gmail.com>
2025-11-05 16:56:35 +0100

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,11 @@
+import pgzrun 
+
+WIDTH = 500
+HEIGHT= 500
+
+def draw():
+    screen.clear()
+    screen.draw.line((50,50),(350,50),"red")
+    screen.draw.line((50,50),(50,350),"green")
+pgzrun.go()    
+
