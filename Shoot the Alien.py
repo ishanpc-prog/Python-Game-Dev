eb3cf379d501d3a5e28f41527e13d4302e82ed9e
ishanpc-prog <ishanpc02@gmail.com>
2025-11-26 16:39:12 +0100

    Add files via upload

--- Shoot the Alien.py
+++ Shoot the Alien.py
@@ -5,10 +5,26 @@ WIDTH  = 500
 HEIGHT = 500
 TITLE  = "Shoot the Alien"
 
-alien_al = Actor("AlienAl") #Actor is used to import the picture of the characters.
+alien_al = Actor("alienal") #Actor is used to import the picture of the characters.
 message = ""
 
 def draw():
     screen.clear()
     screen.fill(color = (150,0,0))
-pgzrun.go()    
+    alien_al.draw()
+    screen.draw.text(message,center= (400,10),fontsize = 25)
+
+def place_alien():
+    alien_al.x = random.randint(50,WIDTH-50)
+    alien_al.y = random.randint(50,HEIGHT-50)
+
+def on_mouse_down(pos):
+    global message
+    if alien_al.collidepoint(pos):
+        message = "Good Shot"
+        place_alien()
+    else:
+        message = "Missed Shot"
+place_alien()            
+
+pgzrun.go()
