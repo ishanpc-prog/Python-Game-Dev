8207a3e3ea96b847c7d888d4933f60315cbd4022
ishanpc-prog <ishanpc02@gmail.com>
2026-01-06 15:48:25 +0100

    Add files via upload

--- bumblebee project.py
+++ bumblebee project.py
@@ -12,7 +12,6 @@ flower.pos = 200,200
 bee.pos = 400,400
 
 score = 0
-
 gameover = False
 
 def draw():
@@ -23,4 +22,29 @@ def draw():
     if gameover == True:
         screen.fill("yellow")
         screen.draw.text("Times up.", color = "black", topleft = (10,10))
-        
+        screen.draw.text("Your score is " + str(score) + ". " + "Congratulations!!!", color = "black", topright = (350,110))
+
+def flower_placement():
+    flower.x = random.randint(70,430)
+    flower.y = random.randint(70,430)
+
+def times_up():
+    global gameover 
+    gameover = True
+
+def update():
+    global score
+    if keyboard.left:
+        bee.x = bee.x - 2
+    if keyboard.right:
+        bee.x = bee.x + 2
+    if keyboard.up:
+        bee.y = bee.y - 2
+    if keyboard.down:
+        bee.y = bee.y + 2
+    flower_collected = bee.colliderect(flower)
+    if flower_collected:
+        score = score + 10
+        flower_placement()
+clock.schedule(times_up,60.0)
+pgzrun.go()
