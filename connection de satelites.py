d80976b307d718e7d2cdacb4a3f2d9621fe1792d
ishanpc-prog <ishanpc02@gmail.com>
2026-01-28 20:56:44 +0100

    Add files via upload

--- connection de satelites.py
+++ connection de satelites.py
@@ -21,3 +21,16 @@ def create_satelite():
         satelite.pos = random.randint(50,750),random.randint(50,550)
         s1.append(satelite)
     start_time = time.time()
+
+def draw():
+    global total_time
+    screen.blit("space",(0,0))
+    number = 1
+    for satelite in s1:
+        screen.draw.text(str(number),(satelite.pos[0],satelite.pos[1]+20))
+        satelite.draw()
+        number = number + 1 
+    for line in lines:
+        screen.draw.line(line[0],line[1],"white")
+    if next_satelite < number_satelites:
+        total_time = time.time() - start_time
