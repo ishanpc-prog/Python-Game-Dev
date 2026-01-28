4cdf9c62e3ab158265a4fc193374604e3a5e1975
ishanpc-prog <ishanpc02@gmail.com>
2026-01-28 20:14:33 +0100

    Add files via upload

---
+++ HW for 28012026 about Tuples.py
@@ -0,0 +1,9 @@
+groupname = str(input("What was the name of the group? "))
+Sizeofthegroup = str(input("What was the size of the group? "))
+dateofthecompetition = str(input("What was the date of the competition? "))
+venue = str(input("What was the venue? "))
+typeofthemedal = str(input("What was the type of medal? "))
+print("")
+
+thainputtuple = (groupname,Sizeofthegroup,dateofthecompetition,venue,typeofthemedal)
+print(thainputtuple)
