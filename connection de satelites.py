33d0eb6aeb78a8f8aacc0be396d2c91552f31608
ishanpc-prog <ishanpc02@gmail.com>
2026-02-02 16:43:51 +0100

    Add files via upload

--- connection de satelites.py
+++ connection de satelites.py
@@ -1,9 +1,9 @@
-import pgzero
+import pgzrun
 import random
 import time
 
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 1000
 TITLE = "Connecting the satelites!!!"
 
 s1 = []
@@ -17,7 +17,7 @@ number_satelites = random.randint(5,16)
 def create_satelite():
     global start_time
     for i in range(0,number_satelites):
-        satelite = Actor("satelite")
+        satelite = Actor("satellite")
         satelite.pos = random.randint(50,750),random.randint(50,550)
         s1.append(satelite)
     start_time = time.time()
@@ -33,4 +33,23 @@ def draw():
     for line in lines:
         screen.draw.line(line[0],line[1],"white")
     if next_satelite < number_satelites:
-        total_time = time.time() - start_time
+        total_time = time.time() - start_time 
+        screen.draw.text("Your time is " + str(total_time) + "!",topleft = (50,50))
+    else:
+        screen.draw.text("Your time is " + str(total_time) + "!",topleft = (50,50))
+
+def on_mouse_down(pos):
+    global next_satelite
+    global lines
+    if next_satelite < number_satelites:
+        if s1[next_satelite].collidepoint(pos):
+            if next_satelite:
+                lines.append((s1[next_satelite-1].pos,s1[next_satelite].pos))
+            next_satelite = next_satelite + 1
+    else:
+        lines = []
+        next_satelite = 0
+
+create_satelite()
+
+pgzrun.go()        
