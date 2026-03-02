57b8045a61d57c6dc9853a3d87279ae2b10d4db9
ishanpc-prog <ishanpc02@gmail.com>
2026-03-02 16:13:25 +0100

    Add files via upload

--- setpanagram.py
+++ setpanagram.py
@@ -1,8 +1,14 @@
 def panagram_check(text):
     s1 = set("abcdefghijklmnopqrstuvwxyz")
     s2 = set()
+    text = text.lower()
+    print("There are " + str(len(text)) + " alphabets in this statement of yours.")
     for alphabet in text:
         if alphabet.isalpha():
             s2.add(alphabet)
-    print(s2)        
-panagram_check("Hello World")
+    print((s2)) 
+    if s2 == s1:
+        print("It is a panagram.")
+    else:
+        print("It is not a panagram.")
+panagram_check(str(input("What is the panagram you wish to write? ")))
