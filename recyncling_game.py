93b71b5434579ea64b732a69dcc13be50f3c1ea1
ishanpc-prog <ishanpc02@gmail.com>
2026-03-09 16:47:40 +0100

    Add files via upload

--- recyncling_game.py
+++ recyncling_game.py
@@ -1,36 +1,41 @@
 import pgzrun
 import random
 
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 800
+HEIGHT = 800
 TITLE = "The Recycling Games"
 
 score = 0
-items = ["paper2","wineglas","bulbli"]
+items = ["bee","satellite","flower"]
 item = Actor(random.choice(items))
 item.pos = random.randint(50,550),0
-trashbin = Actor("trashbin")
+trashbin = Actor("alienal")
 trashbin.pos = 300,550
 
 def draw():
     screen.clear()
-    screen.fill(color = (0,120,120))
     trashbin.draw()
     item.draw()
-    screen.draw.text("The score is " + str(score), color = "black", topleft = (10,10))
+    screen.draw.text("The score is " + str(score), color = "white", topleft = (10,10))
 
 def update():
     global item
     global score
     item.y = item.y + 4
     if keyboard.left:
-        trashbin = trashbin - 2
+        trashbin.x = trashbin.x - 2
     if keyboard.right:
-        trashbin = trashbin + 2
+        trashbin.x = trashbin.x + 2
     if trashbin.colliderect(item):
-        if item.image == "paper2":
+        if item.image == "bee":
             score = score + 10 
-        if item.image == "wineglas":
+        if item.image == "satellite":
             score = score - 10
-        if item.image == "bulbli":
+        if item.image == "flower":
             score = score - 10
+        item = Actor(random.choice(items))
+        item.pos = random.randint(50,550),0
+    if item.y > HEIGHT:
+        item = Actor(random.choice(items))
+        item.pos =random.randint(50,550),0   
+pgzrun.go()
