509d38aa5eaa0752c3312d7851c3b18135225b73
ishanpc-prog <ishanpc02@gmail.com>
2026-03-16 16:18:35 +0100

    Add files via upload

--- healthyeating1.py
+++ healthyeating1.py
@@ -1,36 +1,41 @@
 import pgzrun
 import random
-
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 1200
+HEIGHT = 700
 TITLE = "Tha healthy eating"
 
 score = 0
-items = ["burger1","apple1","pizza1"]
+items = ["bee","appleed","isopizza"]
 item = Actor(random.choice(items))
-item.pos = random.randint(50,550),0
-basket = Actor("basket1")
+item.pos = random.randint(50,1050),0
+basket = Actor("alienal")
 basket.pos = 300,550
 
 def draw():
     screen.clear()
-    screen.fill(color = (0,120,120))
     basket.draw()
     item.draw()
-    screen.draw.text("The score is " + str(score), color = "black", topleft = (10,10))
+    screen.draw.text("The score is " + str(score), color = "white", topleft = (10,10))
 
 def update():
     global item
     global score
-    item.y = item.y + 4
+    item.y = item.y + 7
     if keyboard.left:
-        basket = basket - 2
+        basket.x = basket.x - 20
     if keyboard.right:
-        basket = basket + 2
+        basket.x = basket.x + 20
+
     if basket.colliderect(item):
-        if item.image == "apple1":
+        if item.image == "appleed":
             score = score + 10 
-        if item.image == "pizza1":
+        if item.image == "bee":
             score = score - 10
-        if item.image == "burger1":
+        if item.image == "isopizza":
             score = score - 10
+        item = Actor(random.choice(items))
+        item.pos = random.randint(50,1050),0
+    if item.y > HEIGHT:
+        item = Actor(random.choice(items))
+        item.pos = random.randint(50,1050),0
+pgzrun.go()
